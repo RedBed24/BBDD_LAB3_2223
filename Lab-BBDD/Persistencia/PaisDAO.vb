cdc63e2027610d5dc1c9162076678aeca41774da
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-03-23 09:15:13 +0100

    Añadido PaisDAO

---
+++ Lab-BBDD/Persistencia/PaisDAO.vb
@@ -0,0 +1,17 @@
+﻿Public Class PaisDAO
+
+    Public ReadOnly Property Pais As Collection
+
+    Public Sub New()
+        Me.Pais = New Collection
+    End Sub
+
+    Public Sub LeerTodo()
+        Dim tempPais As Pais
+        Dim tabla, tupla As Collection
+
+        tabla = AgenteBD.obtenerAgente().Leer()
+
+    End Sub
+
+End Class
