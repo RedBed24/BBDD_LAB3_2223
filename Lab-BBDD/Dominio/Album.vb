5a00c640ffad892a62984e1af951a24b2c4fb721
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-07 10:04:25 +0200

    Add AlbumDAO and wrapper in Album

--- Lab-BBDD/Dominio/Album.vb
+++ Lab-BBDD/Dominio/Album.vb
@@ -2,13 +2,56 @@
     Public Property idAlbum As Integer
     Public Property NombreAlbum As String
     Public Property AñoAlbum As Integer
-    Public Property Artista As Artista
+    Public Property Artist As Artista
+
+    Public ReadOnly Property AlbuDAO As AlbumDAO
+
+    Public Sub New()
+        AlbuDAO = New AlbumDAO
+    End Sub
 
     Public Sub New(id As Integer)
+        AlbuDAO = New AlbumDAO
         idAlbum = id
     End Sub
 
-    Public Sub LeerTodosAlbumes(album As String)
-
+    Public Sub New(nomb As String, año As Integer, Artist As Artista)
+        AlbuDAO = New AlbumDAO
+        NombreAlbum = nomb
+        AñoAlbum = año
+        Me.Artist = Artist
     End Sub
+
+    Public Sub New(id As Integer, nomb As String, año As Integer, Artist As Artista)
+        AlbuDAO = New AlbumDAO
+        idAlbum = id
+        NombreAlbum = nomb
+        AñoAlbum = año
+        Me.Artist = Artist
+    End Sub
+
+    Public Sub LeerTodosAlbumes()
+        Me.AlbuDAO.LeerTodo()
+    End Sub
+
+    Public Sub LeerAlbum()
+        Me.AlbuDAO.Leer(Me)
+    End Sub
+
+    Public Function InsertarAlbum() As Integer
+        Return Me.AlbuDAO.Insertar(Me)
+    End Function
+
+    Public Function ActualizarAlbum() As Integer
+        Return Me.AlbuDAO.Actualizar(Me)
+    End Function
+
+    Public Function BorrarAlbum() As Integer
+        Return Me.AlbuDAO.Borrar(Me)
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return idAlbum & ": " & NombreAlbum & ", artist " & Artist.ToString
+    End Function
+
 End Class
---
+++ Lab-BBDD/Persistencia/AlbumDAO.vb
@@ -0,0 +1,58 @@
+﻿Public Class AlbumDAO
+
+    Public ReadOnly Property Albumes As Collection
+
+    Private Shared nombreTabla As String = "albumes"
+    Private Shared campoIdentificador As String = "idAlbum"
+
+    Public Sub New()
+        Me.Albumes = New Collection
+    End Sub
+
+    Public Sub LeerTodo()
+        Dim tempAlbum As Album
+        Dim tempArtista As Artista
+
+        Dim tabla, tupla As Collection
+
+        tabla = AgenteBD.ObtenerAgente().Leer("select * from " & nombreTabla & "")
+
+        For Each tupla In tabla
+            tempArtista = New Artista(Integer.Parse(tupla(4).ToString))
+            tempArtista.LeerArtista()
+
+            tempAlbum = New Album(Integer.Parse(tupla(1).ToString), tupla(2).ToString, Integer.Parse(tupla(3).ToString), tempArtista)
+
+            Me.Albumes.Add(tempAlbum)
+        Next
+    End Sub
+
+    ' TODO: Debería ser Por Valor o Por Referencia?? Se van a ver los datos leídos una vez que hayamos llamado al método?
+    Public Sub Leer(ByVal album As Album)
+        Dim tempArtista As Artista
+
+        Dim tabla As Collection
+
+        tabla = AgenteBD.ObtenerAgente().Leer("select * from " & nombreTabla & " where " & campoIdentificador & "='" & album.idAlbum & "';")
+
+        tempArtista = New Artista(Integer.Parse(tabla(1)(4).ToString))
+        tempArtista.LeerArtista()
+
+        album.NombreAlbum = tabla(1)(2).ToString
+        album.AñoAlbum = Integer.Parse(tabla(1)(3).ToString)
+        album.Artist = tempArtista
+    End Sub
+
+    Public Function Insertar(ByVal album As Album) As Integer
+        ' No le pasamos una clave porque esta la genera la bbdd automáticamente
+        Return AgenteBD.ObtenerAgente().Modificar("insert into " & nombreTabla & " (NombreAlbum, AñoAlbum, Artista) values ('" & album.NombreAlbum & "', '" & album.AñoAlbum & "', '" & album.Artist.IdArtista & "');")
+    End Function
+    Public Function Actualizar(ByVal album As Album) As Integer
+        Return AgenteBD.ObtenerAgente().Modificar("update " & nombreTabla & " set NombreAlbum='" & album.NombreAlbum & "', AñoAlbum='" & album.AñoAlbum & "', Artista='" & album.Artist.IdArtista & "' where " & campoIdentificador & "='" & album.idAlbum & "';")
+    End Function
+
+    Public Function Borrar(ByVal album As Album) As Integer
+        Return AgenteBD.ObtenerAgente().Modificar("delete from " & nombreTabla & " where " & campoIdentificador & "='" & album.idAlbum & "';")
+    End Function
+
+End Class
