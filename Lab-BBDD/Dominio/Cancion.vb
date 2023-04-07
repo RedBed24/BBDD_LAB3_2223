f5bb98733f1c1d23f0b14ad8c754b786a8f72b15
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-07 10:08:29 +0200

    Add CancionDAO and wrapper in Cancion

--- Lab-BBDD/Dominio/Cancion.vb
+++ Lab-BBDD/Dominio/Cancion.vb
@@ -6,4 +6,56 @@
 
     Public Property Album As Album
 
+    Public ReadOnly Property CancioDAO As CancionDAO
+
+    Public Sub New()
+        CancioDAO = New CancionDAO
+    End Sub
+
+    Public Sub New(id As Integer)
+        CancioDAO = New CancionDAO
+        idCancion = id
+    End Sub
+
+    Public Sub New(nomb As String, dur As Integer, Albu As Album, orden As Integer)
+        CancioDAO = New CancionDAO
+        NombreCancion = nomb
+        Duracion = dur
+        Me.Album = Albu
+        OrdenCancion = orden
+    End Sub
+
+    Public Sub New(id As Integer, nomb As String, dur As Integer, Albu As Album, orden As Integer)
+        CancioDAO = New CancionDAO
+        idCancion = id
+        NombreCancion = nomb
+        Duracion = dur
+        Me.Album = Albu
+        OrdenCancion = orden
+    End Sub
+
+    Public Sub LeerTodosCanciones()
+        Me.CancioDAO.LeerTodo()
+    End Sub
+
+    Public Sub LeerCancion()
+        Me.CancioDAO.Leer(Me)
+    End Sub
+
+    Public Function InsertarCancion() As Integer
+        Return Me.CancioDAO.Insertar(Me)
+    End Function
+
+    Public Function ActualizarCancion() As Integer
+        Return Me.CancioDAO.Actualizar(Me)
+    End Function
+
+    Public Function BorrarCancion() As Integer
+        Return Me.CancioDAO.Borrar(Me)
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return idCancion & ": " & NombreCancion & ", artist " & Album.ToString
+    End Function
+
 End Class
---
+++ Lab-BBDD/Persistencia/CancionDAO.vb
@@ -0,0 +1,58 @@
+﻿Public Class CancionDAO
+    Public ReadOnly Property Canciones As Collection
+
+    Private Shared nombreTabla As String = "canciones"
+    Private Shared campoIdentificador As String = "idCancion"
+
+    Public Sub New()
+        Me.Canciones = New Collection
+    End Sub
+
+    Public Sub LeerTodo()
+        Dim tempCancion As Cancion
+        Dim tempAlbum As Album
+
+        Dim tabla, tupla As Collection
+
+        tabla = AgenteBD.ObtenerAgente().Leer("select * from " & nombreTabla & "")
+
+        For Each tupla In tabla
+            tempAlbum = New Album(Integer.Parse(tupla(4).ToString))
+            tempAlbum.LeerAlbum()
+
+            tempCancion = New Cancion(Integer.Parse(tupla(1).ToString), tupla(2).ToString, Integer.Parse(tupla(3).ToString), tempAlbum, Integer.Parse(tupla(5).ToString))
+
+            Me.Canciones.Add(tempCancion)
+        Next
+    End Sub
+
+    ' TODO: Debería ser Por Valor o Por Referencia?? Se van a ver los datos leídos una vez que hayamos llamado al método?
+    Public Sub Leer(ByVal cancion As Cancion)
+        Dim tempAlbum As Album
+
+        Dim tabla As Collection
+
+        tabla = AgenteBD.ObtenerAgente().Leer("select * from " & nombreTabla & " where " & campoIdentificador & "='" & cancion.idCancion & "';")
+
+        tempAlbum = New Album(Integer.Parse(tabla(1)(4).ToString))
+        tempAlbum.LeerAlbum()
+
+        cancion.NombreCancion = tabla(1)(2).ToString
+        cancion.Duracion = Integer.Parse(tabla(1)(3).ToString)
+        cancion.Album = tempAlbum
+        cancion.OrdenCancion = Integer.Parse(tabla(1)(3).ToString)
+    End Sub
+
+    Public Function Insertar(ByVal cancion As Cancion) As Integer
+        ' No le pasamos una clave porque esta la genera la bbdd automáticamente
+        Return AgenteBD.ObtenerAgente().Modificar("insert into " & nombreTabla & " (NombreCancion, Duración, Album, OrdenCanción) values ('" & cancion.NombreCancion & "', '" & cancion.Duracion & "', '" & cancion.Album.idAlbum & "', '" & cancion.OrdenCancion & "');")
+    End Function
+    Public Function Actualizar(ByVal cancion As Cancion) As Integer
+        Return AgenteBD.ObtenerAgente().Modificar("update " & nombreTabla & " set NombreCancion='" & cancion.NombreCancion & "', Duración='" & cancion.Duracion & "', Album='" & cancion.Album.idAlbum & ", OrdenCanción='" & cancion.OrdenCancion & "' where " & campoIdentificador & "='" & cancion.idCancion & "';")
+    End Function
+
+    Public Function Borrar(ByVal cancion As Cancion) As Integer
+        Return AgenteBD.ObtenerAgente().Modificar("delete from " & nombreTabla & " where " & campoIdentificador & "='" & cancion.idCancion & "';")
+    End Function
+
+End Class
