6108ad431d69c261fded28938a4896cc014ed146
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-08 10:10:27 +0200

    Add Select, Insert y Delete a formularios Artistas y Paises

--- Lab-BBDD/Presentacion/FormularioPrincipal.vb
+++ Lab-BBDD/Presentacion/FormularioPrincipal.vb
@@ -1,6 +1,106 @@
 ﻿Public Class FormularioPrincipal
 
-    Private Sub Paises_ButtonLimpiar_Click(sender As Object, e As EventArgs) Handles Paises_ButtonLimpiar.Click
+    '
+    ' TAB ARTISTA
+    '
+
+    Private Sub Artista_Button_VerTodosArtistas_Click(sender As Object, e As EventArgs) Handles Artista_Button_VerTodosArtistas.Click
+        Artista_Button_Limpiar.PerformClick()
+
+        Dim artistas As New Artista
+
+        Try
+            artistas.LeerTodosArtistas()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Sub
+        End Try
+
+        For Each artista As Artista In artistas.ArtistDAO.Artistas
+            Artista_ListBox_Artistas.Items.Add(artista)
+        Next
+
+    End Sub
+
+    Private Sub Artista_Button_Agregar_Click(sender As Object, e As EventArgs) Handles Artista_Button_Agregar.Click
+        Dim nombre As String = Artista_TextBox_NombreArtista.Text
+
+        If String.IsNullOrEmpty(nombre) Then
+            MessageBox.Show("El artista debe tener un nombre.")
+            Exit Sub
+        End If
+
+        ' TODO: Obtener el país??
+        Dim paisartista As New Pais("ESP")
+        paisartista.LeerPais()
+
+        Dim artistaanadir As New Artista(nombre, paisartista)
+
+        Try
+            If artistaanadir.InsertarArtista() <> 1 Then
+                MessageBox.Show("wtf cómo puede ocurrir esto?")
+            End If
+        Catch ex As Exception
+            ' no se puede añadir
+            MessageBox.Show(ex.Message)
+            Exit Sub
+        End Try
+
+        MessageBox.Show(artistaanadir.Nombre & " añadido correctamente.")
+        Artista_ListBox_Artistas.Items.Add(artistaanadir)
+        Artista_ListBox_Artistas.SelectedItem = artistaanadir
+
+    End Sub
+
+    Private Sub Artista_Button_Eliminar_Click(sender As Object, e As EventArgs) Handles Artista_Button_Eliminar.Click
+        If Artista_ListBox_Artistas.SelectedItem Is Nothing Then
+            MessageBox.Show("Se debe seleccionar un Artista")
+            Exit Sub
+        End If
+
+        Dim artistaborrar As Artista = Artista_ListBox_Artistas.SelectedItem
+
+        Try
+            If artistaborrar.BorrarArtista() <> 1 Then
+                MessageBox.Show("no existe (y no se puede borrar) " & artistaborrar.ToString)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Sub
+        End Try
+
+        MessageBox.Show(artistaborrar.ToString & " eliminado correctamente")
+        Artista_ListBox_Artistas.Items.Remove(artistaborrar)
+
+    End Sub
+
+    Private Sub Artista_Button_Limpiar_Click(sender As Object, e As EventArgs) Handles Artista_Button_Limpiar.Click
+        Artista_ListBox_Artistas.Items.Clear()
+
+        Artista_TextBox_NombreArtista.Clear()
+        Artista_TextBox_PaisArtista.Clear()
+
+    End Sub
+
+    Private Sub Artista_ListBox_Artistas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Artista_ListBox_Artistas.SelectedIndexChanged
+        If Artista_ListBox_Artistas.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim artista As Artista = Artista_ListBox_Artistas.SelectedItem
+
+        Artista_TextBox_NombreArtista.Text = artista.Nombre
+        Artista_TextBox_PaisArtista.Text = artista.Pais.NombrePais
+
+    End Sub
+
+    '
+    ' TAB PAISES
+    '
+
+    Private Sub Paises_Button_VerTodosPaises_Click(sender As Object, e As EventArgs) Handles Paises_Button_VerTodosPaises.Click
+        Paises_Button_Limpiar.PerformClick()
+
         Dim paises As New Pais
 
         Try
@@ -11,25 +111,33 @@
         End Try
 
         For Each pais As Pais In paises.PaiDAO.Paises
-            MessageBox.Show(pais.ToString)
+            Paises_ListBox_Paises.Items.Add(pais)
         Next
 
     End Sub
 
-    Private Sub Paises_ButtonAgregar_Click(sender As Object, e As EventArgs) Handles Paises_ButtonAgregar.Click
-        Dim cod, nombre As String
+    Private Sub Paises_Button_Limpiar_Click(sender As Object, e As EventArgs) Handles Paises_Button_Limpiar.Click
+        Paises_ListBox_Paises.Items.Clear()
 
-        nombre = "España"
-        cod = Pais.obtainID(nombre)
+        Paises_TextBox_NombrePais.Clear()
+    End Sub
+
+    Private Sub Paises_Button_Agregar_Click(sender As Object, e As EventArgs) Handles Paises_Button_Agregar.Click
+        Dim nombre As String = Paises_TextBox_NombrePais.Text
+
+        If String.IsNullOrEmpty(nombre) Then
+            MessageBox.Show("El país debe tener un nombre.")
+            Exit Sub
+        End If
+
+        Dim cod As String = Pais.obtainID(nombre)
 
         Dim paisanadir As New Pais(cod, nombre)
 
-
         Try
-            If paisanadir.InsertarPais() = 1 Then
-                MessageBox.Show(paisanadir.ToString & " añadido correctamente")
-            Else
+            If paisanadir.InsertarPais() <> 1 Then
                 MessageBox.Show("wtf cómo puede ocurrir esto?")
+                Exit Sub
             End If
         Catch ex As Exception
             ' no se puede añadir
@@ -37,82 +145,45 @@
             Exit Sub
         End Try
 
+        MessageBox.Show(paisanadir.ToString & " añadido correctamente")
+        Paises_ListBox_Paises.Items.Add(paisanadir)
+        Paises_ListBox_Paises.SelectedItem = paisanadir
+
     End Sub
 
-    Private Sub Paises_ButtonEliminar_Click(sender As Object, e As EventArgs) Handles Paises_ButtonEliminar.Click
-        Dim cod, nombre As String
+    Private Sub Paises_Button_Eliminar_Click(sender As Object, e As EventArgs) Handles Paises_Button_Eliminar.Click
+        If Paises_ListBox_Paises.SelectedItem Is Nothing Then
+            MessageBox.Show("Se debe seleccionar un País")
+            Exit Sub
+        End If
 
-        nombre = "España"
-        cod = Pais.obtainID(nombre)
-
-        Dim paisborrar As New Pais(cod, nombre)
+        Dim paisborrar As Pais = Paises_ListBox_Paises.SelectedItem
 
         Try
-            If paisborrar.BorrarPais() = 1 Then
-                MessageBox.Show(paisborrar.ToString & " eliminado correctamente")
-            Else
+            If paisborrar.BorrarPais() <> 1 Then
+                ' wtf como ocurre esto?
                 MessageBox.Show("no existe (y no se puede borrar) " & paisborrar.ToString)
+                Exit Sub
             End If
         Catch ex As Exception
             MessageBox.Show(ex.Message)
             Exit Sub
         End Try
 
-    End Sub
-
-    Private Sub Artista_ButtonLimpiar_Click(sender As Object, e As EventArgs) Handles Artista_ButtonLimpiar.Click
-        Dim artistas As New Artista
-
-        Try
-            artistas.LeerTodosArtistas()
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-            Exit Sub
-        End Try
-
-        For Each pais As Artista In artistas.ArtistDAO.Artistas
-            MessageBox.Show(pais.ToString)
-        Next
-    End Sub
-
-    Private Sub Artista_Button_Agregar_Click(sender As Object, e As EventArgs) Handles Artista_Button_Agregar.Click
-        Dim paisartista As New Pais("ESP")
-        paisartista.LeerPais()
-
-        Dim artistaanadir As New Artista("Samuel", paisartista)
-
-        Try
-            If artistaanadir.InsertarArtista() = 1 Then
-                MessageBox.Show(artistaanadir.ToString & " añadido correctamente EL ID NO COINCIDE CON EL QUE LE HA PUESTO LA DB")
-            Else
-                MessageBox.Show("wtf cómo puede ocurrir esto?")
-            End If
-        Catch ex As Exception
-            ' no se puede añadir
-            MessageBox.Show(ex.Message)
-            Exit Sub
-        End Try
+        MessageBox.Show(paisborrar.ToString & " eliminado correctamente")
+        Paises_ListBox_Paises.Items.Remove(paisborrar)
 
     End Sub
 
-    Private Sub Artista_ButtonEliminar_Click(sender As Object, e As EventArgs) Handles Artista_ButtonEliminar.Click
-        Dim paisartista As New Pais("ESP")
-        paisartista.LeerPais()
-
-        Dim artistaborrar As New Artista(Integer.Parse("1"), "Samuel", paisartista)
-
-        Try
-            If artistaborrar.BorrarArtista() = 1 Then
-                MessageBox.Show(artistaborrar.ToString & " eliminado correctamente")
-            Else
-                MessageBox.Show("no existe (y no se puede borrar) " & artistaborrar.ToString)
-            End If
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
+    Private Sub Paises_ListBox_Paises_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Paises_ListBox_Paises.SelectedIndexChanged
+        If Paises_ListBox_Paises.SelectedItem Is Nothing Then
             Exit Sub
-        End Try
+        End If
+
+        Dim pais As Pais = Paises_ListBox_Paises.SelectedItem
+
+        Paises_TextBox_NombrePais.Text = pais.NombrePais
 
     End Sub
 
 End Class
-'Hay que usar combobox para las clases de paises, sitios, artistas, albumes, canciones y conciertos
