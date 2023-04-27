41277ed66304b6c96e3dc535c90d7bfba9ff6793
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-27 14:13:48 +0200

    Fix SQL AlbumDAO.LeerAlbumesMasInterpretados(artista) uknown table album (+es)

--- Lab-BBDD/Persistencia/AlbumDAO.vb
+++ Lab-BBDD/Persistencia/AlbumDAO.vb
@@ -60,7 +60,7 @@
 
         tabla = AgenteBD.ObtenerAgente().Leer("
             Select count(s.Concierto) || ': ' || a.NombreAlbum
-            from album a, canciones c, setlists s
+            from albumes a, canciones c, setlists s
             where a.idAlbum = c.Album and c.idCancion = s.canción
             group by a.idAlbum
             order by count(s.Concierto) desc;
