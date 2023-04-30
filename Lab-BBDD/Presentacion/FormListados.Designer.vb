489c075107b673260a8ae717dcd5c458753d011f
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-30 09:22:24 +0200

    Move FormListados para que quepa nuevo ListBox

--- Lab-BBDD/Presentacion/FormListados.Designer.vb
+++ Lab-BBDD/Presentacion/FormListados.Designer.vb
@@ -38,17 +38,17 @@ Partial Class FormListados
         'Listados_ComboBox_Artista
         '
         Me.Listados_ComboBox_Artista.FormattingEnabled = True
-        Me.Listados_ComboBox_Artista.Location = New System.Drawing.Point(15, 30)
-        Me.Listados_ComboBox_Artista.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Listados_ComboBox_Artista.Location = New System.Drawing.Point(11, 24)
+        Me.Listados_ComboBox_Artista.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Listados_ComboBox_Artista.Name = "Listados_ComboBox_Artista"
-        Me.Listados_ComboBox_Artista.Size = New System.Drawing.Size(255, 24)
+        Me.Listados_ComboBox_Artista.Size = New System.Drawing.Size(192, 21)
         Me.Listados_ComboBox_Artista.TabIndex = 0
         '
         'Listados_ListBox_CancionesMasInterpretadasArtista
         '
         Me.Listados_ListBox_CancionesMasInterpretadasArtista.FormattingEnabled = True
         Me.Listados_ListBox_CancionesMasInterpretadasArtista.Location = New System.Drawing.Point(11, 69)
-        Me.Listados_ListBox_CancionesMasInterpretadasArtista.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Listados_ListBox_CancionesMasInterpretadasArtista.Margin = New System.Windows.Forms.Padding(2)
         Me.Listados_ListBox_CancionesMasInterpretadasArtista.Name = "Listados_ListBox_CancionesMasInterpretadasArtista"
         Me.Listados_ListBox_CancionesMasInterpretadasArtista.Size = New System.Drawing.Size(192, 82)
         Me.Listados_ListBox_CancionesMasInterpretadasArtista.TabIndex = 1
@@ -56,76 +56,66 @@ Partial Class FormListados
         'Listados_ListBox_AlbumesMasInterpretados
         '
         Me.Listados_ListBox_AlbumesMasInterpretados.FormattingEnabled = True
-        Me.Listados_ListBox_AlbumesMasInterpretados.ItemHeight = 16
-        Me.Listados_ListBox_AlbumesMasInterpretados.Location = New System.Drawing.Point(15, 222)
-        Me.Listados_ListBox_AlbumesMasInterpretados.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Listados_ListBox_AlbumesMasInterpretados.Location = New System.Drawing.Point(11, 180)
+        Me.Listados_ListBox_AlbumesMasInterpretados.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Listados_ListBox_AlbumesMasInterpretados.Name = "Listados_ListBox_AlbumesMasInterpretados"
-        Me.Listados_ListBox_AlbumesMasInterpretados.Size = New System.Drawing.Size(255, 132)
+        Me.Listados_ListBox_AlbumesMasInterpretados.Size = New System.Drawing.Size(192, 108)
         Me.Listados_ListBox_AlbumesMasInterpretados.TabIndex = 2
         '
         'Listados_ListBox_ArtistasAlbumCompleto
         '
         Me.Listados_ListBox_ArtistasAlbumCompleto.FormattingEnabled = True
-        Me.Listados_ListBox_ArtistasAlbumCompleto.ItemHeight = 16
-        Me.Listados_ListBox_ArtistasAlbumCompleto.Location = New System.Drawing.Point(300, 30)
-        Me.Listados_ListBox_ArtistasAlbumCompleto.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Listados_ListBox_ArtistasAlbumCompleto.Location = New System.Drawing.Point(225, 24)
+        Me.Listados_ListBox_ArtistasAlbumCompleto.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Listados_ListBox_ArtistasAlbumCompleto.Name = "Listados_ListBox_ArtistasAlbumCompleto"
-        Me.Listados_ListBox_ArtistasAlbumCompleto.Size = New System.Drawing.Size(167, 324)
+        Me.Listados_ListBox_ArtistasAlbumCompleto.Size = New System.Drawing.Size(126, 121)
         Me.Listados_ListBox_ArtistasAlbumCompleto.TabIndex = 3
         '
         'Listados_DateTimePicker_FechaInicio
         '
-        Me.Listados_DateTimePicker_FechaInicio.Location = New System.Drawing.Point(489, 31)
-        Me.Listados_DateTimePicker_FechaInicio.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Listados_DateTimePicker_FechaInicio.Location = New System.Drawing.Point(367, 25)
         Me.Listados_DateTimePicker_FechaInicio.Name = "Listados_DateTimePicker_FechaInicio"
-        Me.Listados_DateTimePicker_FechaInicio.Size = New System.Drawing.Size(253, 22)
+        Me.Listados_DateTimePicker_FechaInicio.Size = New System.Drawing.Size(191, 20)
         Me.Listados_DateTimePicker_FechaInicio.TabIndex = 5
         '
         'Listados_DateTimePicker_FechaFin
         '
-        Me.Listados_DateTimePicker_FechaFin.Location = New System.Drawing.Point(489, 85)
-        Me.Listados_DateTimePicker_FechaFin.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Listados_DateTimePicker_FechaFin.Location = New System.Drawing.Point(367, 69)
         Me.Listados_DateTimePicker_FechaFin.Name = "Listados_DateTimePicker_FechaFin"
-        Me.Listados_DateTimePicker_FechaFin.Size = New System.Drawing.Size(253, 22)
+        Me.Listados_DateTimePicker_FechaFin.Size = New System.Drawing.Size(191, 20)
         Me.Listados_DateTimePicker_FechaFin.TabIndex = 6
         '
         'Listados_ListBox_ArtistasMasConcietos
         '
         Me.Listados_ListBox_ArtistasMasConcietos.FormattingEnabled = True
-        Me.Listados_ListBox_ArtistasMasConcietos.ItemHeight = 16
-        Me.Listados_ListBox_ArtistasMasConcietos.Location = New System.Drawing.Point(489, 137)
-        Me.Listados_ListBox_ArtistasMasConcietos.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Listados_ListBox_ArtistasMasConcietos.Location = New System.Drawing.Point(367, 111)
         Me.Listados_ListBox_ArtistasMasConcietos.Name = "Listados_ListBox_ArtistasMasConcietos"
-        Me.Listados_ListBox_ArtistasMasConcietos.Size = New System.Drawing.Size(253, 84)
+        Me.Listados_ListBox_ArtistasMasConcietos.Size = New System.Drawing.Size(191, 69)
         Me.Listados_ListBox_ArtistasMasConcietos.TabIndex = 7
         '
         'Listados_ListBox_PaisesMasConciertos
         '
         Me.Listados_ListBox_PaisesMasConciertos.FormattingEnabled = True
-        Me.Listados_ListBox_PaisesMasConciertos.ItemHeight = 16
-        Me.Listados_ListBox_PaisesMasConciertos.Location = New System.Drawing.Point(489, 254)
-        Me.Listados_ListBox_PaisesMasConciertos.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Listados_ListBox_PaisesMasConciertos.Location = New System.Drawing.Point(367, 206)
         Me.Listados_ListBox_PaisesMasConciertos.Name = "Listados_ListBox_PaisesMasConciertos"
-        Me.Listados_ListBox_PaisesMasConciertos.Size = New System.Drawing.Size(253, 100)
+        Me.Listados_ListBox_PaisesMasConciertos.Size = New System.Drawing.Size(191, 82)
         Me.Listados_ListBox_PaisesMasConciertos.TabIndex = 8
         '
         'Listados_Label_FiltrarArtista
         '
         Me.Listados_Label_FiltrarArtista.AutoSize = True
-        Me.Listados_Label_FiltrarArtista.Location = New System.Drawing.Point(16, 11)
-        Me.Listados_Label_FiltrarArtista.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Listados_Label_FiltrarArtista.Location = New System.Drawing.Point(12, 9)
         Me.Listados_Label_FiltrarArtista.Name = "Listados_Label_FiltrarArtista"
-        Me.Listados_Label_FiltrarArtista.Size = New System.Drawing.Size(128, 16)
+        Me.Listados_Label_FiltrarArtista.Size = New System.Drawing.Size(102, 13)
         Me.Listados_Label_FiltrarArtista.TabIndex = 9
         Me.Listados_Label_FiltrarArtista.Text = "Artista a seleccionar"
         '
         'Listados_Label_CancionesMasInterpretadas
         '
         Me.Listados_Label_CancionesMasInterpretadas.AutoSize = True
-        Me.Listados_Label_CancionesMasInterpretadas.Location = New System.Drawing.Point(16, 66)
-        Me.Listados_Label_CancionesMasInterpretadas.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Listados_Label_CancionesMasInterpretadas.Location = New System.Drawing.Point(12, 54)
         Me.Listados_Label_CancionesMasInterpretadas.Name = "Listados_Label_CancionesMasInterpretadas"
-        Me.Listados_Label_CancionesMasInterpretadas.Size = New System.Drawing.Size(243, 16)
+        Me.Listados_Label_CancionesMasInterpretadas.Size = New System.Drawing.Size(191, 13)
         Me.Listados_Label_CancionesMasInterpretadas.TabIndex = 10
         Me.Listados_Label_CancionesMasInterpretadas.Text = "Canciones mas interpretadas del artista"
         '
@@ -133,15 +123,16 @@ Partial Class FormListados
         '
         Me.Listados_ListBox_CancionesMasIntepretadas.FormattingEnabled = True
         Me.Listados_ListBox_CancionesMasIntepretadas.Location = New System.Drawing.Point(225, 180)
+        Me.Listados_ListBox_CancionesMasIntepretadas.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Listados_ListBox_CancionesMasIntepretadas.Name = "Listados_ListBox_CancionesMasIntepretadas"
         Me.Listados_ListBox_CancionesMasIntepretadas.Size = New System.Drawing.Size(126, 108)
         Me.Listados_ListBox_CancionesMasIntepretadas.TabIndex = 11
         '
         'FormListados
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(760, 368)
+        Me.ClientSize = New System.Drawing.Size(570, 299)
         Me.Controls.Add(Me.Listados_ListBox_CancionesMasIntepretadas)
         Me.Controls.Add(Me.Listados_Label_CancionesMasInterpretadas)
         Me.Controls.Add(Me.Listados_Label_FiltrarArtista)
@@ -154,7 +145,7 @@ Partial Class FormListados
         Me.Controls.Add(Me.Listados_ListBox_CancionesMasInterpretadasArtista)
         Me.Controls.Add(Me.Listados_ComboBox_Artista)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FormListados"
         Me.Text = "FormListados"
         Me.ResumeLayout(False)
