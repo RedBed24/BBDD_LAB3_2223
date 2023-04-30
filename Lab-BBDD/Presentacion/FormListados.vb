64caa716d93218600d04c52016e69e6332c98dad
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-30 15:32:18 +0200

    Add Llamada últimos listados

--- Lab-BBDD/Presentacion/FormListados.vb
+++ Lab-BBDD/Presentacion/FormListados.vb
@@ -17,6 +17,9 @@
 
         Dim artista As Artista = Listados_ComboBox_Artista.SelectedItem
 
+        Listados_ListBox_CancionesMasInterpretadasArtista.Items.Clear()
+        Listados_ListBox_AlbumesMasInterpretados.Items.Clear()
+
         Try
             CargarCancionesMasInterpretadas(artista)
             CargarAlbumesMasInterpretados(artista)
@@ -24,6 +27,23 @@
             MessageBox.Show(ex.ToString)
         End Try
 
+    End Sub
+
+    Private Sub Listados_DateTimePicker_FechaInicio_ValueChanged(sender As Object, e As EventArgs) Handles Listados_DateTimePicker_FechaInicio.ValueChanged
+        LimpiarYCargarListadosFecha()
+    End Sub
+
+    Private Sub Listados_DateTimePicker_FechaFin_ValueChanged(sender As Object, e As EventArgs) Handles Listados_DateTimePicker_FechaFin.ValueChanged
+        LimpiarYCargarListadosFecha()
+    End Sub
+
+    Private Sub LimpiarYCargarListadosFecha()
+        Try
+            CargarArtistasMasConciertos(Listados_DateTimePicker_FechaInicio.Value, Listados_DateTimePicker_FechaFin.Value)
+            CargarPaiesMasConciertos(Listados_DateTimePicker_FechaInicio.Value, Listados_DateTimePicker_FechaFin.Value)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
     End Sub
 
     Private Sub CargarArtistas()
@@ -73,6 +93,26 @@
 
         For Each album As Album In albumes.AlbuDAO.Albumes
             Listados_ListBox_AlbumesMasInterpretados.Items.Add(artista)
+        Next
+    End Sub
+
+    Private Sub CargarArtistasMasConciertos(fechaInicio As Date, fechaFin As Date)
+        Dim artistas As New Artista
+
+        artistas.LeerArtistasMasConciertos(fechaInicio, fechaFin)
+
+        For Each artista In artistas.ArtistDAO.Artistas
+            Listados_ListBox_ArtistasMasConcietos.Items.Add(artista)
+        Next
+    End Sub
+
+    Private Sub CargarPaiesMasConciertos(fechaInicio As Date, fechaFin As Date)
+        Dim paises As New Pais
+
+        paises.LeerPaisesMasConciertos(fechaInicio, fechaFin)
+
+        For Each pais In paises.PaiDAO.Paises
+            Listados_ListBox_PaisesMasConciertos.Items.Add(pais)
         Next
     End Sub
 
