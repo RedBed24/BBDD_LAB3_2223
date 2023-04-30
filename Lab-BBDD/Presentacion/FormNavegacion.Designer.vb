8cf3278d72e2c53316f7a8f15837db8a4eb7959a
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-30 12:39:15 +0200

    Add Navegacion completa

--- Lab-BBDD/Presentacion/FormNavegacion.Designer.vb
+++ Lab-BBDD/Presentacion/FormNavegacion.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormNavegacion
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,127 +20,157 @@ Partial Class FormNavegacion
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Navegacion_ListBox_Conciertos = New System.Windows.Forms.ListBox()
-        Me.Navegacion_Button_VerListado = New System.Windows.Forms.Button()
-        Me.Albumes_Button_Limpiar = New System.Windows.Forms.Button()
-        Me.Navegacion_LabelNombreArtista = New System.Windows.Forms.Label()
-        Me.Navegacion_ComboBoxArtistas = New System.Windows.Forms.ComboBox()
-        Me.lblTitulo_Nav = New System.Windows.Forms.Label()
-        Me.Navegacion_Label_Sitio = New System.Windows.Forms.Label()
+        Me.Navegacion_Label_TituloForm = New System.Windows.Forms.Label()
+        Me.Navegacion_ListBox_ConciertosNav1 = New System.Windows.Forms.ListBox()
+        Me.Navegacion_ListBox_CancionesConcierto = New System.Windows.Forms.ListBox()
+        Me.Navegacion_ComboBox_Artista = New System.Windows.Forms.ComboBox()
+        Me.Navegacion_ComboBox_Pais = New System.Windows.Forms.ComboBox()
         Me.Navegacion_ComboBox_Sitio = New System.Windows.Forms.ComboBox()
-        Me.Navegacion_ListBox_setlist = New System.Windows.Forms.ListBox()
+        Me.Navegaciones_CheckBox_FiltrarPais = New System.Windows.Forms.CheckBox()
+        Me.Navegacion_ListBox_ConciertosNav2 = New System.Windows.Forms.ListBox()
+        Me.Navegacion_Label_Artista = New System.Windows.Forms.Label()
+        Me.Navegaciones_Label_ConciertosNav1 = New System.Windows.Forms.Label()
+        Me.Navegaciones_Label_ConciertosNav2 = New System.Windows.Forms.Label()
+        Me.Navegacion_Label_CancionesConcierto = New System.Windows.Forms.Label()
+        Me.Navegacion_Label_Sitio = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Navegacion_ListBox_Conciertos
+        'Navegacion_Label_TituloForm
         '
-        Me.Navegacion_ListBox_Conciertos.BackColor = System.Drawing.Color.White
-        Me.Navegacion_ListBox_Conciertos.FormattingEnabled = True
-        Me.Navegacion_ListBox_Conciertos.ItemHeight = 16
-        Me.Navegacion_ListBox_Conciertos.Location = New System.Drawing.Point(12, 139)
-        Me.Navegacion_ListBox_Conciertos.Name = "Navegacion_ListBox_Conciertos"
-        Me.Navegacion_ListBox_Conciertos.Size = New System.Drawing.Size(223, 308)
-        Me.Navegacion_ListBox_Conciertos.TabIndex = 56
-        Me.Navegacion_ListBox_Conciertos.UseWaitCursor = True
+        Me.Navegacion_Label_TituloForm.AutoSize = True
+        Me.Navegacion_Label_TituloForm.Font = New System.Drawing.Font("Britannic Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Navegacion_Label_TituloForm.Location = New System.Drawing.Point(231, 65)
+        Me.Navegacion_Label_TituloForm.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Navegacion_Label_TituloForm.Name = "Navegacion_Label_TituloForm"
+        Me.Navegacion_Label_TituloForm.Size = New System.Drawing.Size(113, 21)
+        Me.Navegacion_Label_TituloForm.TabIndex = 0
+        Me.Navegacion_Label_TituloForm.Text = "NAVEGACION"
         '
-        'Navegacion_Button_VerListado
+        'Navegacion_ListBox_ConciertosNav1
         '
-        Me.Navegacion_Button_VerListado.BackColor = System.Drawing.Color.MistyRose
-        Me.Navegacion_Button_VerListado.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Navegacion_Button_VerListado.Location = New System.Drawing.Point(12, 96)
-        Me.Navegacion_Button_VerListado.Name = "Navegacion_Button_VerListado"
-        Me.Navegacion_Button_VerListado.Size = New System.Drawing.Size(110, 37)
-        Me.Navegacion_Button_VerListado.TabIndex = 55
-        Me.Navegacion_Button_VerListado.Text = "Ver listado" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        Me.Navegacion_Button_VerListado.UseVisualStyleBackColor = False
-        Me.Navegacion_Button_VerListado.UseWaitCursor = True
+        Me.Navegacion_ListBox_ConciertosNav1.FormattingEnabled = True
+        Me.Navegacion_ListBox_ConciertosNav1.Location = New System.Drawing.Point(12, 140)
+        Me.Navegacion_ListBox_ConciertosNav1.Name = "Navegacion_ListBox_ConciertosNav1"
+        Me.Navegacion_ListBox_ConciertosNav1.Size = New System.Drawing.Size(162, 147)
+        Me.Navegacion_ListBox_ConciertosNav1.TabIndex = 1
         '
-        'Albumes_Button_Limpiar
+        'Navegacion_ListBox_CancionesConcierto
         '
-        Me.Albumes_Button_Limpiar.Location = New System.Drawing.Point(492, 330)
-        Me.Albumes_Button_Limpiar.Margin = New System.Windows.Forms.Padding(2)
-        Me.Albumes_Button_Limpiar.Name = "Albumes_Button_Limpiar"
-        Me.Albumes_Button_Limpiar.Size = New System.Drawing.Size(106, 35)
-        Me.Albumes_Button_Limpiar.TabIndex = 54
-        Me.Albumes_Button_Limpiar.Text = "Limpiar"
-        Me.Albumes_Button_Limpiar.UseVisualStyleBackColor = True
+        Me.Navegacion_ListBox_CancionesConcierto.FormattingEnabled = True
+        Me.Navegacion_ListBox_CancionesConcierto.Location = New System.Drawing.Point(205, 140)
+        Me.Navegacion_ListBox_CancionesConcierto.Name = "Navegacion_ListBox_CancionesConcierto"
+        Me.Navegacion_ListBox_CancionesConcierto.Size = New System.Drawing.Size(162, 147)
+        Me.Navegacion_ListBox_CancionesConcierto.TabIndex = 2
         '
-        'Navegacion_LabelNombreArtista
+        'Navegacion_ComboBox_Artista
         '
-        Me.Navegacion_LabelNombreArtista.AutoSize = True
-        Me.Navegacion_LabelNombreArtista.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Navegacion_LabelNombreArtista.Location = New System.Drawing.Point(540, 126)
-        Me.Navegacion_LabelNombreArtista.Name = "Navegacion_LabelNombreArtista"
-        Me.Navegacion_LabelNombreArtista.Size = New System.Drawing.Size(128, 16)
-        Me.Navegacion_LabelNombreArtista.TabIndex = 53
-        Me.Navegacion_LabelNombreArtista.Text = "Selecciona el artista"
-        Me.Navegacion_LabelNombreArtista.UseWaitCursor = True
+        Me.Navegacion_ComboBox_Artista.FormattingEnabled = True
+        Me.Navegacion_ComboBox_Artista.Location = New System.Drawing.Point(12, 31)
+        Me.Navegacion_ComboBox_Artista.Name = "Navegacion_ComboBox_Artista"
+        Me.Navegacion_ComboBox_Artista.Size = New System.Drawing.Size(162, 21)
+        Me.Navegacion_ComboBox_Artista.TabIndex = 3
         '
-        'Navegacion_ComboBoxArtistas
+        'Navegacion_ComboBox_Pais
         '
-        Me.Navegacion_ComboBoxArtistas.FormattingEnabled = True
-        Me.Navegacion_ComboBoxArtistas.Location = New System.Drawing.Point(532, 155)
-        Me.Navegacion_ComboBoxArtistas.Name = "Navegacion_ComboBoxArtistas"
-        Me.Navegacion_ComboBoxArtistas.Size = New System.Drawing.Size(150, 24)
-        Me.Navegacion_ComboBoxArtistas.TabIndex = 52
-        Me.Navegacion_ComboBoxArtistas.UseWaitCursor = True
-        '
-        'lblTitulo_Nav
-        '
-        Me.lblTitulo_Nav.AutoSize = True
-        Me.lblTitulo_Nav.Font = New System.Drawing.Font("Britannic Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTitulo_Nav.Location = New System.Drawing.Point(327, 32)
-        Me.lblTitulo_Nav.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblTitulo_Nav.Name = "lblTitulo_Nav"
-        Me.lblTitulo_Nav.Size = New System.Drawing.Size(138, 26)
-        Me.lblTitulo_Nav.TabIndex = 51
-        Me.lblTitulo_Nav.Text = "NAVEGACION"
-        '
-        'Navegacion_Label_Sitio
-        '
-        Me.Navegacion_Label_Sitio.AutoSize = True
-        Me.Navegacion_Label_Sitio.Location = New System.Drawing.Point(540, 206)
-        Me.Navegacion_Label_Sitio.Name = "Navegacion_Label_Sitio"
-        Me.Navegacion_Label_Sitio.Size = New System.Drawing.Size(116, 16)
-        Me.Navegacion_Label_Sitio.TabIndex = 58
-        Me.Navegacion_Label_Sitio.Text = "Sitio del concierto:"
-        Me.Navegacion_Label_Sitio.UseWaitCursor = True
+        Me.Navegacion_ComboBox_Pais.FormattingEnabled = True
+        Me.Navegacion_ComboBox_Pais.Location = New System.Drawing.Point(12, 92)
+        Me.Navegacion_ComboBox_Pais.Name = "Navegacion_ComboBox_Pais"
+        Me.Navegacion_ComboBox_Pais.Size = New System.Drawing.Size(162, 21)
+        Me.Navegacion_ComboBox_Pais.TabIndex = 4
         '
         'Navegacion_ComboBox_Sitio
         '
         Me.Navegacion_ComboBox_Sitio.FormattingEnabled = True
-        Me.Navegacion_ComboBox_Sitio.Location = New System.Drawing.Point(532, 225)
+        Me.Navegacion_ComboBox_Sitio.Location = New System.Drawing.Point(396, 31)
         Me.Navegacion_ComboBox_Sitio.Name = "Navegacion_ComboBox_Sitio"
-        Me.Navegacion_ComboBox_Sitio.Size = New System.Drawing.Size(202, 24)
-        Me.Navegacion_ComboBox_Sitio.TabIndex = 57
-        Me.Navegacion_ComboBox_Sitio.UseWaitCursor = True
+        Me.Navegacion_ComboBox_Sitio.Size = New System.Drawing.Size(162, 21)
+        Me.Navegacion_ComboBox_Sitio.TabIndex = 5
         '
-        'Navegacion_ListBox_setlist
+        'Navegaciones_CheckBox_FiltrarPais
         '
-        Me.Navegacion_ListBox_setlist.BackColor = System.Drawing.Color.White
-        Me.Navegacion_ListBox_setlist.FormattingEnabled = True
-        Me.Navegacion_ListBox_setlist.ItemHeight = 16
-        Me.Navegacion_ListBox_setlist.Location = New System.Drawing.Point(259, 219)
-        Me.Navegacion_ListBox_setlist.Name = "Navegacion_ListBox_setlist"
-        Me.Navegacion_ListBox_setlist.Size = New System.Drawing.Size(190, 228)
-        Me.Navegacion_ListBox_setlist.TabIndex = 59
-        Me.Navegacion_ListBox_setlist.UseWaitCursor = True
+        Me.Navegaciones_CheckBox_FiltrarPais.AutoSize = True
+        Me.Navegaciones_CheckBox_FiltrarPais.Location = New System.Drawing.Point(12, 69)
+        Me.Navegaciones_CheckBox_FiltrarPais.Name = "Navegaciones_CheckBox_FiltrarPais"
+        Me.Navegaciones_CheckBox_FiltrarPais.Size = New System.Drawing.Size(133, 17)
+        Me.Navegaciones_CheckBox_FiltrarPais.TabIndex = 6
+        Me.Navegaciones_CheckBox_FiltrarPais.Text = "Filtrar también por país"
+        Me.Navegaciones_CheckBox_FiltrarPais.UseVisualStyleBackColor = True
+        '
+        'Navegacion_ListBox_ConciertosNav2
+        '
+        Me.Navegacion_ListBox_ConciertosNav2.FormattingEnabled = True
+        Me.Navegacion_ListBox_ConciertosNav2.Location = New System.Drawing.Point(396, 140)
+        Me.Navegacion_ListBox_ConciertosNav2.Name = "Navegacion_ListBox_ConciertosNav2"
+        Me.Navegacion_ListBox_ConciertosNav2.Size = New System.Drawing.Size(162, 147)
+        Me.Navegacion_ListBox_ConciertosNav2.TabIndex = 7
+        '
+        'Navegacion_Label_Artista
+        '
+        Me.Navegacion_Label_Artista.AutoSize = True
+        Me.Navegacion_Label_Artista.Location = New System.Drawing.Point(12, 15)
+        Me.Navegacion_Label_Artista.Name = "Navegacion_Label_Artista"
+        Me.Navegacion_Label_Artista.Size = New System.Drawing.Size(36, 13)
+        Me.Navegacion_Label_Artista.TabIndex = 8
+        Me.Navegacion_Label_Artista.Text = "Artista"
+        '
+        'Navegaciones_Label_ConciertosNav1
+        '
+        Me.Navegaciones_Label_ConciertosNav1.AutoSize = True
+        Me.Navegaciones_Label_ConciertosNav1.Location = New System.Drawing.Point(12, 124)
+        Me.Navegaciones_Label_ConciertosNav1.Name = "Navegaciones_Label_ConciertosNav1"
+        Me.Navegaciones_Label_ConciertosNav1.Size = New System.Drawing.Size(57, 13)
+        Me.Navegaciones_Label_ConciertosNav1.TabIndex = 9
+        Me.Navegaciones_Label_ConciertosNav1.Text = "Conciertos"
+        '
+        'Navegaciones_Label_ConciertosNav2
+        '
+        Me.Navegaciones_Label_ConciertosNav2.AutoSize = True
+        Me.Navegaciones_Label_ConciertosNav2.Location = New System.Drawing.Point(393, 124)
+        Me.Navegaciones_Label_ConciertosNav2.Name = "Navegaciones_Label_ConciertosNav2"
+        Me.Navegaciones_Label_ConciertosNav2.Size = New System.Drawing.Size(57, 13)
+        Me.Navegaciones_Label_ConciertosNav2.TabIndex = 10
+        Me.Navegaciones_Label_ConciertosNav2.Text = "Conciertos"
+        '
+        'Navegacion_Label_CancionesConcierto
+        '
+        Me.Navegacion_Label_CancionesConcierto.AutoSize = True
+        Me.Navegacion_Label_CancionesConcierto.Location = New System.Drawing.Point(202, 124)
+        Me.Navegacion_Label_CancionesConcierto.Name = "Navegacion_Label_CancionesConcierto"
+        Me.Navegacion_Label_CancionesConcierto.Size = New System.Drawing.Size(121, 13)
+        Me.Navegacion_Label_CancionesConcierto.TabIndex = 11
+        Me.Navegacion_Label_CancionesConcierto.Text = "Canciones del concierto"
+        '
+        'Navegacion_Label_Sitio
+        '
+        Me.Navegacion_Label_Sitio.AutoSize = True
+        Me.Navegacion_Label_Sitio.Location = New System.Drawing.Point(393, 15)
+        Me.Navegacion_Label_Sitio.Name = "Navegacion_Label_Sitio"
+        Me.Navegacion_Label_Sitio.Size = New System.Drawing.Size(27, 13)
+        Me.Navegacion_Label_Sitio.TabIndex = 12
+        Me.Navegacion_Label_Sitio.Text = "Sitio"
         '
         'FormNavegacion
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Navegacion_ListBox_setlist)
+        Me.ClientSize = New System.Drawing.Size(570, 299)
         Me.Controls.Add(Me.Navegacion_Label_Sitio)
+        Me.Controls.Add(Me.Navegacion_Label_CancionesConcierto)
+        Me.Controls.Add(Me.Navegaciones_Label_ConciertosNav2)
+        Me.Controls.Add(Me.Navegaciones_Label_ConciertosNav1)
+        Me.Controls.Add(Me.Navegacion_Label_Artista)
+        Me.Controls.Add(Me.Navegacion_ListBox_ConciertosNav2)
+        Me.Controls.Add(Me.Navegaciones_CheckBox_FiltrarPais)
         Me.Controls.Add(Me.Navegacion_ComboBox_Sitio)
-        Me.Controls.Add(Me.Navegacion_ListBox_Conciertos)
-        Me.Controls.Add(Me.Navegacion_Button_VerListado)
-        Me.Controls.Add(Me.Albumes_Button_Limpiar)
-        Me.Controls.Add(Me.Navegacion_LabelNombreArtista)
-        Me.Controls.Add(Me.Navegacion_ComboBoxArtistas)
-        Me.Controls.Add(Me.lblTitulo_Nav)
+        Me.Controls.Add(Me.Navegacion_ComboBox_Pais)
+        Me.Controls.Add(Me.Navegacion_ComboBox_Artista)
+        Me.Controls.Add(Me.Navegacion_ListBox_CancionesConcierto)
+        Me.Controls.Add(Me.Navegacion_ListBox_ConciertosNav1)
+        Me.Controls.Add(Me.Navegacion_Label_TituloForm)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "FormNavegacion"
         Me.Text = "FormNavegacion"
         Me.ResumeLayout(False)
@@ -148,13 +178,17 @@ Partial Class FormNavegacion
 
     End Sub
 
-    Friend WithEvents Navegacion_ListBox_Conciertos As ListBox
-    Friend WithEvents Navegacion_Button_VerListado As Button
-    Friend WithEvents Albumes_Button_Limpiar As Button
-    Friend WithEvents Navegacion_LabelNombreArtista As Label
-    Friend WithEvents Navegacion_ComboBoxArtistas As ComboBox
-    Friend WithEvents lblTitulo_Nav As Label
-    Friend WithEvents Navegacion_Label_Sitio As Label
+    Friend WithEvents Navegacion_Label_TituloForm As Label
+    Friend WithEvents Navegacion_ListBox_ConciertosNav1 As ListBox
+    Friend WithEvents Navegacion_ListBox_CancionesConcierto As ListBox
+    Friend WithEvents Navegacion_ComboBox_Artista As ComboBox
+    Friend WithEvents Navegacion_ComboBox_Pais As ComboBox
     Friend WithEvents Navegacion_ComboBox_Sitio As ComboBox
-    Friend WithEvents Navegacion_ListBox_setlist As ListBox
+    Friend WithEvents Navegaciones_CheckBox_FiltrarPais As CheckBox
+    Friend WithEvents Navegacion_ListBox_ConciertosNav2 As ListBox
+    Friend WithEvents Navegacion_Label_Artista As Label
+    Friend WithEvents Navegaciones_Label_ConciertosNav1 As Label
+    Friend WithEvents Navegaciones_Label_ConciertosNav2 As Label
+    Friend WithEvents Navegacion_Label_CancionesConcierto As Label
+    Friend WithEvents Navegacion_Label_Sitio As Label
 End Class
--- Lab-BBDD/Presentacion/FormNavegacion.vb
+++ Lab-BBDD/Presentacion/FormNavegacion.vb
@@ -1,33 +1,143 @@
 ﻿Public Class FormNavegacion
+
     Private Sub FormNavegacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            CargarArtistas()
+            CargarPaises()
+            CargarSitios()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
 
     End Sub
 
-    Private Sub lblTitulo_Nav_Click(sender As Object, e As EventArgs) Handles lblTitulo_Nav.Click
+    Private Sub CargarArtistas()
+        Dim artistas As New Artista
+
+        artistas.LeerTodosArtistas()
+
+        For Each artista As Artista In artistas.ArtistDAO.Artistas
+            Navegacion_ComboBox_Artista.Items.Add(artista)
+        Next
+    End Sub
+
+    Private Sub CargarPaises()
+        Dim paises As New Pais
+
+        paises.LeerTodosPaises()
+
+        For Each pais As Pais In paises.PaiDAO.Paises
+            Navegacion_ComboBox_Pais.Items.Add(pais)
+        Next
+    End Sub
+
+    Private Sub CargarSitios()
+        Dim sitios As New Sitio
+
+        sitios.LeerTodosSitios()
+
+        For Each sitio As Sitio In sitios.SiteDAO.Sitios
+            Navegacion_ComboBox_Sitio.Items.Add(sitio)
+        Next
+    End Sub
+
+    Private Sub CargarCanciones(concierto As Concierto)
+        Navegacion_ListBox_CancionesConcierto.Items.Clear()
+
+        For Each cancion As Cancion In concierto.SetList
+            Navegacion_ListBox_CancionesConcierto.Items.Add(cancion)
+        Next
 
     End Sub
 
-    Private Sub Albumes_Button_Limpiar_Click(sender As Object, e As EventArgs) Handles Albumes_Button_Limpiar.Click
-        Navegacion_ListBox_Conciertos.Items.Clear()
-        Navegacion_ListBox_setlist.Items.Clear()
+    Private Sub Navegacion1()
+        Navegacion_ListBox_ConciertosNav1.Items.Clear()
 
-    End Sub
+        If Navegacion_ComboBox_Artista.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
 
-    Private Sub Navegacion_Button_VerListado_Click(sender As Object, e As EventArgs) Handles Navegacion_Button_VerListado.Click
-        Navegacion_Button_VerListado.PerformClick()
+        Dim artista As Artista = Navegacion_ComboBox_Artista.SelectedItem
+
+        Dim filtrarPorPais As Boolean = Navegaciones_CheckBox_FiltrarPais.Checked
+
+        If filtrarPorPais And Navegacion_ComboBox_Pais.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim pais As Pais = Navegacion_ComboBox_Pais.SelectedItem
 
         Dim conciertos As New Concierto
 
         Try
-            conciertos.LeerTodosConciertoes()
+            If filtrarPorPais Then
+                conciertos.LeerConciertosArtistaPais(artista, pais)
+            Else
+                conciertos.LeerConciertosArtista(artista)
+            End If
         Catch ex As Exception
-            MessageBox.Show(ex.Message)
-            Exit Sub
+            MessageBox.Show(ex.ToString)
         End Try
 
-        For Each album As Album In conciertos.ConciertDAO.Conciertos
-
-            Navegacion_ListBox_Conciertos.Items.Add(conciertos)
+        For Each concierto As Concierto In conciertos.ConciertDAO.Conciertos
+            Navegacion_ListBox_ConciertosNav2.Items.Add(concierto)
         Next
+
     End Sub
+
+    Private Sub Navegacion_ComboBox_Sitio_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Navegacion_ComboBox_Sitio.SelectedIndexChanged
+        Navegacion_ListBox_ConciertosNav2.Items.Clear()
+
+        If Navegacion_ComboBox_Sitio.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim sitio As Sitio = Navegacion_ComboBox_Sitio.SelectedItem
+
+        Dim conciertos As New Concierto
+
+        Try
+            conciertos.LeerConciertosSitio(sitio)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+
+        For Each concierto As Concierto In conciertos.ConciertDAO.Conciertos
+            Navegacion_ListBox_ConciertosNav2.Items.Add(concierto)
+        Next
+
+    End Sub
+
+    Private Sub Navegacion_ComboBox_Artista_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Navegacion_ComboBox_Artista.SelectedIndexChanged
+        Navegacion1()
+    End Sub
+
+    Private Sub Navegaciones_CheckBox_FiltrarPais_CheckedChanged(sender As Object, e As EventArgs) Handles Navegaciones_CheckBox_FiltrarPais.CheckedChanged
+        Navegacion1()
+    End Sub
+
+    Private Sub Navegacion_ComboBox_Pais_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Navegacion_ComboBox_Pais.SelectedIndexChanged
+        Navegacion1()
+    End Sub
+
+    Private Sub Navegacion_ListBox_ConciertosNav1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Navegacion_ListBox_ConciertosNav1.SelectedIndexChanged
+        If Navegacion_ListBox_ConciertosNav1.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim concierto As Concierto = Navegacion_ListBox_ConciertosNav1.SelectedItem
+
+        CargarCanciones(concierto)
+    End Sub
+
+    Private Sub Navegacion_ListBox_ConciertosNav2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Navegacion_ListBox_ConciertosNav2.SelectedIndexChanged
+        If Navegacion_ListBox_ConciertosNav2.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim concierto As Concierto = Navegacion_ListBox_ConciertosNav2.SelectedItem
+
+        CargarCanciones(concierto)
+    End Sub
+
 End Class
